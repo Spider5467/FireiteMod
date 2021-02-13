1880648fa5256268ede8d5bbc75b972daf4c31a3
Spider5467 <SpiderJayden555@gmail.com>
2021-02-13 12:16:37 -0600

    textures

--- src/main/java/com/example/fireitemod/setup/ModItems.java
+++ src/main/java/com/example/fireitemod/setup/ModItems.java
@@ -1,5 +1,7 @@
 package com.example.fireitemod.setup;
 
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -10,13 +12,13 @@ public class ModItems {
     public static final RegistryObject<Item> FIREITE_INGOT = Registration.ITEMS.register("fireite_ingot", () ->
             new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
     public static final RegistryObject<Item> FIREITE_HELMET = Registration.ITEMS.register("fireite_helmet", () ->
-            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+            new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.HEAD, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
     public static final RegistryObject<Item> FIREITE_CHESTPLATE = Registration.ITEMS.register("fireite_chestplate", () ->
-            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+            new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.CHEST, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
     public static final RegistryObject<Item> FIREITE_LEGGINGS = Registration.ITEMS.register("fireite_leggings", () ->
-            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+            new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.LEGS, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
     public static final RegistryObject<Item> FIREITE_BOOTS = Registration.ITEMS.register("fireite_boots", () ->
-            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+            new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.FEET, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
 
     public static void initialize() {
     }
