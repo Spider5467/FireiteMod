ffaa1a1132c91d01a78d18243e22d12b36884850
Spider5467 <SpiderJayden555@gmail.com>
2021-02-13 14:51:19 -0600

    recipys

--- src/main/java/com/example/fireitemod/setup/ModItems.java
+++ src/main/java/com/example/fireitemod/setup/ModItems.java
@@ -11,13 +11,13 @@ public class ModItems {
 
     public static final RegistryObject<Item> FIREITE_INGOT = Registration.ITEMS.register("fireite_ingot", () ->
             new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
-    public static final RegistryObject<Item> FIREITE_HELMET = Registration.ITEMS.register("fireite_helmet", () ->
+    public static final RegistryObject<Item> FIREITE_HELMET = Registration.ITEMS.register("fireite_helmet.json", () ->
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.HEAD, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
-    public static final RegistryObject<Item> FIREITE_CHESTPLATE = Registration.ITEMS.register("fireite_chestplate", () ->
+    public static final RegistryObject<Item> FIREITE_CHESTPLATE = Registration.ITEMS.register("fireite_chestplate.json.json", () ->
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.CHEST, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
-    public static final RegistryObject<Item> FIREITE_LEGGINGS = Registration.ITEMS.register("fireite_leggings", () ->
+    public static final RegistryObject<Item> FIREITE_LEGGINGS = Registration.ITEMS.register("fireite_leggings.json", () ->
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.LEGS, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
-    public static final RegistryObject<Item> FIREITE_BOOTS = Registration.ITEMS.register("fireite_boots", () ->
+    public static final RegistryObject<Item> FIREITE_BOOTS = Registration.ITEMS.register("fireite_boots.json", () ->
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.FEET, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS)));
     public static final RegistryObject<Item> FIREITE_SWORD = Registration.ITEMS.register("fireite_sword", () ->
             new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
