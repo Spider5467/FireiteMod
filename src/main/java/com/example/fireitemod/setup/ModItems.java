e1516d012bd340fac5f71daf8e0c02a739b0ee8e
Spider5467 <SpiderJayden555@gmail.com>
2021-02-12 18:05:32 -0600

    Add images

--- src/main/java/com/example/fireitemod/setup/ModItems.java
+++ src/main/java/com/example/fireitemod/setup/ModItems.java
@@ -9,6 +9,14 @@ public class ModItems {
 
     public static final RegistryObject<Item> FIREITE_INGOT = Registration.ITEMS.register("fireite_ingot", () ->
             new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+    public static final RegistryObject<Item> FIREITE_HELMET = Registration.ITEMS.register("fireite_helmet", () ->
+            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+    public static final RegistryObject<Item> FIREITE_CHESTPLATE = Registration.ITEMS.register("fireite_chestplate", () ->
+            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+    public static final RegistryObject<Item> FIREITE_LEGGINGS = Registration.ITEMS.register("fireite_leggings", () ->
+            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
+    public static final RegistryObject<Item> FIREITE_BOOTS = Registration.ITEMS.register("fireite_boots", () ->
+            new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS)));
 
     public static void initialize() {
     }
