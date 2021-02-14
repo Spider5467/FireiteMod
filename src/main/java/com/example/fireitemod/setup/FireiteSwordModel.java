3806d82573d377b775097fb0e4f2209e0bd26b7b
Spider5467 <SpiderJayden555@gmail.com>
2021-02-14 08:38:05 -0600

    Models

---
+++ src/main/java/com/example/fireitemod/setup/FireiteSwordModel.java
@@ -0,0 +1,13 @@
+package com.example.fireitemod.setup;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.SwordItem;
+
+public class FireiteSwordModel extends SwordItem {
+
+
+
+    public FireiteSwordModel(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builderIn) {
+        super(tier, attackDamageIn, attackSpeedIn, builderIn);
+    }
+}
--- src/main/java/com/example/fireitemod/setup/ModItems.java
+++ src/main/java/com/example/fireitemod/setup/ModItems.java
@@ -19,7 +19,7 @@ public class ModItems {
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.LEGS, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS).isImmuneToFire()));
     public static final RegistryObject<Item> FIREITE_BOOTS = Registration.ITEMS.register("fireite_boots", () ->
             new ArmorItem(ArmorMaterialList.fireite, EquipmentSlotType.FEET, new ArmorItem.Properties().group(ModItemGroups.FIREITE_ITEMS).isImmuneToFire()));
-    public static final RegistryObject<Item> FIREITE_SWORD = Registration.ITEMS.register("fireite_sword", () ->
+    public static final RegistryObject<Item> FIREITE_SWORD = Registration.ITEMS.register("fireite_sword",  () ->
             new Item(new Item.Properties().group(ModItemGroups.FIREITE_ITEMS).isImmuneToFire()));
     public static final RegistryObject<Item> FIREITE_PICKAXE = Registration.ITEMS.register("fireite_pickaxe", FireitePickaxeModel::new);
     public static final RegistryObject<Item> FIREITE_AXE = Registration.ITEMS.register("fireite_axe", () ->
