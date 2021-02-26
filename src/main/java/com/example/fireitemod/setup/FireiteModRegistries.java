1eee8a24740e074ae9f292568b045b8337fa34a7
Jayden <spiderjayden555@gmail.com>
2021-02-26 15:58:08 -0600

    do not create

---
+++ src/main/java/com/example/fireitemod/setup/FireiteModRegistries.java
@@ -0,0 +1,13 @@
+package com.example.fireitemod.setup;
+
+import com.example.fireitemod.setup.ModItemGroups;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
+
+public class FireiteModRegistries {
+    public static final ItemGroup FIREITE_ITEMS = ModItemGroups.FIREITE_ITEMS;
+
+    public static ResourceLocation location(Object name) {
+        return null;
+    }
+}
